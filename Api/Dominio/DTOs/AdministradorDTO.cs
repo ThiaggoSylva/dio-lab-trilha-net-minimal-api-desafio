20b7202b7e0bb4a9dc50b5d82436923cc5e29c0f
Thiaggo Sylva <thiaggol.sylva@gmail.com>
2025-10-07 20:36:53 -0300

    Terminando o Projeto

--- Api/Dominio/DTOs/AdministradorDTO.cs
+++ Api/Dominio/DTOs/AdministradorDTO.cs
@@ -6,12 +6,13 @@ using dio_lab_trilha_net_minimal_api_desafio.Dominio.Enums;
 
 namespace dio_lab_trilha_net_minimal_api_desafio.Dominio.DTOs
 {
+    //
     public class AdministradorDTO
     {
         public string Email { get; set; }
 
         public string Senha { get; set; }
-        
+
         public Perfil Perfil { get; set; }
     }
 }
